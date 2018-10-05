e6627980b5e1aa05d5d1259d2193ae93291d0375
Tim Sweet <timothyswt@gmail.com>
2018-10-05 11:51:57 -0700

    adding fake language data

--- src/index.js
+++ src/index.js
@@ -7,13 +7,26 @@ import "../node_modules/react-resizable/css/styles.css";
 import { IntlProvider, addLocaleData } from "react-intl";
 import locale_en from "react-intl/locale-data/en";
 import locale_de from "react-intl/locale-data/de";
+import locale_es from "react-intl/locale-data/es";
+import locale_fr from "react-intl/locale-data/fr";
+import locale_it from "react-intl/locale-data/it";
+import locale_ru from "react-intl/locale-data/ru";
 
-import messages_de from "./translations/de.json";
 import messages_en from "./translations/en.json";
+import messages_de from "./translations/de.json";
+import messages_es from "./translations/es.json";
+import messages_fr from "./translations/fr.json";
+import messages_it from "./translations/it.json";
+import messages_ru from "./translations/ru.json";
 
 const messages = {
+  en: messages_en,
   de: messages_de,
-  en: messages_en
+  es: messages_es,
+  fr: messages_fr,
+  it: messages_it,
+  ru: messages_ru,
+  
 };
 const language = navigator.language.split(/[-_]/)[0]; // language without region code
 
@@ -30,4 +43,11 @@ ReactDOM.render(
   document.getElementById("root")
 );
 registerServiceWorker();
-addLocaleData([...locale_en, ...locale_de]);
+addLocaleData([
+  ...locale_en, 
+  ...locale_de, 
+  ...locale_es, 
+  ...locale_fr, 
+  ...locale_it, 
+  ...locale_ru
+]);
