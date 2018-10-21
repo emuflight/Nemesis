577f08ffc4725736afe58747894d2e5a70c6fef4
Tim Sweet <timothyswt@gmail.com>
2018-10-21 13:31:33 -0700

    fixing profile selection

--- src/Views/ProfileView/ProfileView.js
+++ src/Views/ProfileView/ProfileView.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ConfigListView from "../ConfigListView/ConfigListView";
 import HelperSelect from "../Items/HelperSelect";
 import Paper from "@material-ui/core/Paper";
-import { FCConfigContext } from "../../App";
 
 export default class ProfileView extends Component {
   constructor(props) {
@@ -25,29 +24,19 @@ export default class ProfileView extends Component {
 
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Paper>
-          <FCConfigContext.Consumer>
-            {config => {
-              const item = config[this.props.id];
-              item.current = parseInt(item.current, 10);
-              return (
-                <HelperSelect
-                  id={this.props.id}
-                  className={this.props.id}
-                  label={this.props.id}
-                  value={item.current}
-                  onChange={event =>
-                    this.props.changeProfile(event.target.value)
-                  }
-                  items={this.props.profileList}
-                />
-              );
-            }}
-          </FCConfigContext.Consumer>
+          <HelperSelect
+            id={this.props.id}
+            className={this.props.id}
+            label={this.props.id}
+            value={this.props.active}
+            onChange={event => this.props.changeProfile(event.target.value)}
+            items={this.props.profileList}
+          />
         </Paper>
         {this.children}
-      </div>
+      </React.Fragment>
     );
   }
 }
