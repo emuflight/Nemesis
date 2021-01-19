092db47214664b8ffcbf0523f8a510929a7c002a
Michael Richo <mricho495@gmail.com>
2021-01-19 01:06:56 -0500

    Do not allow closing of cli during sending bulk commands

--- src/Views/CliView/CliView.js
+++ src/Views/CliView/CliView.js
@@ -14,20 +14,23 @@ export default class CliView extends Component {
       cliBuffer: this.props.startText || "#flashEmu\n\n#",
       stayOpen: !!this.props.stayOpen || false,
       disabled: this.props.disabled || false,
-      open: this.props.stayOpen || !!this.props.open
+      open: this.props.stayOpen || !!this.props.open,
+      allowClose: true
     };
   }
   toggleCli(state) {
     let openState = this.state.stayOpen || state;
-    this.setState({
-      cliBuffer: this.props.startText || "#flashEmu\n\n#",
-      command: "",
-      open: openState
-    });
-    if (openState) {
-      FCConnector.pauseTelemetry();
-    } else {
-      FCConnector.resumeTelemetry();
+    if (openState || this.state.allowClose) {
+      this.setState({
+        cliBuffer: this.props.startText || "#flashEmu\n\n#",
+        command: "",
+        open: openState
+      });
+      if (openState) {
+        FCConnector.pauseTelemetry();
+      } else {
+        FCConnector.resumeTelemetry();
+      }
     }
   }
   replaceLast(update) {
@@ -64,6 +67,7 @@ export default class CliView extends Component {
       this.setState({ disabled: true });
       if (this.state.command) {
         let commands = this.state.command.split(/\r|\n/gim).filter(com => com);
+        this.setState({ allowClose: false });
         FCConnector.sendBulkCommands(
           commands.filter(line => !line.startsWith("#")),
           notifyResp => {
@@ -71,6 +75,7 @@ export default class CliView extends Component {
           }
         ).then(() => {
           this.setState({ disabled: false });
+          this.setState({ allowClose: true });
         });
       }
     }
