f64a2855758db4be226b2b28efb28f0990ec3596
Michael Richo <mricho495@gmail.com>
2021-02-18 21:48:53 -0500

    before converting from class to function

--- src/Views/AuxChannelView/NewAuxChannelView.js
+++ src/Views/AuxChannelView/NewAuxChannelView.js
@@ -5,6 +5,55 @@ import Paper from "@material-ui/core/Paper";
 import FCConnector from "../../utilities/FCConnector";
 import { FormattedMessage } from "react-intl";
 import Typography from "@material-ui/core/Typography";
+
+//==============
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion"; // move this to component
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionActions from "@material-ui/core/AccordionActions";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Chip from "@material-ui/core/Chip";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import clsx from "clsx";
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15)
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  },
+  icon: {
+    verticalAlign: "bottom",
+    height: 20,
+    width: 20
+  },
+  details: {
+    alignItems: "center"
+  },
+  column: {
+    flexBasis: "33.33%"
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: theme.spacing(1, 2)
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline"
+    }
+  }
+}));
+const classes = useStyles(); // move to component
+//==============
+
 // <AuxChannelView
 // fcConfig={mergedProfile}
 // auxScale={mergedProfile.rx_scale}
@@ -41,7 +90,8 @@ export default class NewAuxChannelView extends Component {
       });
     }
     FCConnector.webSockets.addEventListener("message", this.handleRXData);
-    FCConnector.startTelemetry("rx");
+    //temp disabled for debugging - allows modes in react view to not refresh constantly
+    //FCConnector.startTelemetry("rx");
   }
 
   componentWillUnmount() {
@@ -51,19 +101,64 @@ export default class NewAuxChannelView extends Component {
 
   render() {
     return (
-      <List>
-        {this.props.auxModeList &&
-          this.props.auxModeList.map((mode, i) => {
-            return (
-              <Typography style={{ marginBottom: 12 }} color="textSecondary">
-                <FormattedMessage
-                  id={mode.label}
-                  //values={{ version: this.props.appVersion }}
-                />
-              </Typography>
-            );
-          })}
-      </List>
+      <Paper>
+        <List>
+          {this.props.auxModeList &&
+            this.props.auxModeList.map((mode, i) => {
+              return (
+                <Accordion defaultExpanded>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header"
+                  >
+                    <div className={classes.column}>
+                      <Typography className={classes.heading}>
+                        Location
+                      </Typography>
+                    </div>
+                    <div className={classes.column}>
+                      <Typography className={classes.secondaryHeading}>
+                        Select trip destination
+                      </Typography>
+                    </div>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.details}>
+                    <div className={classes.column} />
+                    <div className={classes.column}>
+                      <Chip label="Barbados" onDelete={() => {}} />
+                    </div>
+                    <div className={clsx(classes.column, classes.helper)}>
+                      <Typography variant="caption">
+                        Select your destination of choice
+                        <br />
+                        <a
+                          href="#secondary-heading-and-columns"
+                          className={classes.link}
+                        >
+                          Learn more
+                        </a>
+                      </Typography>
+                    </div>
+                  </AccordionDetails>
+                  <Divider />
+                  <AccordionActions>
+                    <Button size="small">Cancel</Button>
+                    <Button size="small" color="primary">
+                      Save
+                    </Button>
+                  </AccordionActions>
+                  <Typography
+                    style={{ marginBottom: 12 }}
+                    color="textSecondary"
+                  >
+                    <FormattedMessage id={mode.label} />
+                  </Typography>
+                </Accordion>
+              );
+            })}
+        </List>
+      </Paper>
     );
   }
 }
