3417520b2955d05bd7149688190cc7d6bdb8839c
Michael Richo <mricho495@gmail.com>
2021-02-19 00:27:10 -0500

    fix css

--- src/Views/AuxChannelView/NewAuxChannelView.js
+++ src/Views/AuxChannelView/NewAuxChannelView.js
@@ -5,7 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import FCConnector from "../../utilities/FCConnector";
 import { FormattedMessage } from "react-intl";
 import Typography from "@material-ui/core/Typography";
-
+import "./NewAuxChannelView.css";
 //==============
 import { makeStyles } from "@material-ui/core/styles";
 import { withStyles } from "@material-ui/styles";
@@ -18,40 +18,6 @@ import Chip from "@material-ui/core/Chip";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import clsx from "clsx";
-const classes = makeStyles(theme => ({
-  root: {
-    width: "100%"
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15)
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary
-  },
-  icon: {
-    verticalAlign: "bottom",
-    height: 20,
-    width: 20
-  },
-  details: {
-    alignItems: "center"
-  },
-  column: {
-    flexBasis: "33.33%"
-  },
-  helper: {
-    borderLeft: `2px solid ${theme.palette.divider}`,
-    padding: theme.spacing(1, 2)
-  },
-  link: {
-    color: theme.palette.primary.main,
-    textDecoration: "none",
-    "&:hover": {
-      textDecoration: "underline"
-    }
-  }
-}));
 
 //==============
 
@@ -65,7 +31,7 @@ const classes = makeStyles(theme => ({
 // }
 // />
 
-class NewAuxChannelView extends Component {
+export default class NewAuxChannelView extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -143,29 +109,29 @@ class NewAuxChannelView extends Component {
                     aria-controls="panel1c-content"
                     id="panel1c-header"
                   >
-                    <div className={classes.column}>
-                      <Typography className={classes.heading}>
+                    <div className="column">
+                      <Typography className="heading">
                         <FormattedMessage id={auxMode.label} />
                       </Typography>
                     </div>
-                    <div className={classes.column}>
-                      <Typography className={classes.secondaryHeading}>
+                    <div className="column">
+                      <Typography className="secondaryHeading">
                         <FormattedMessage id="aux.select.channel" />
                       </Typography>
                     </div>
                   </AccordionSummary>
-                  <AccordionDetails className={classes.details}>
-                    <div className={classes.column} />
-                    <div className={classes.column}>
+                  <AccordionDetails className="details">
+                    <div className="column" />
+                    <div className="column">
                       <Chip label="{mode.channel}" onDelete={() => {}} />
                     </div>
-                    <div className={clsx(classes.column, classes.helper)}>
+                    <div className="column helper">
                       <Typography variant="caption">
                         Explanation of flight mode
                         <br />
                         <a
                           href="#secondary-heading-and-columns"
-                          className={classes.link}
+                          className="link"
                         >
                           Learn more
                         </a>
@@ -187,4 +153,3 @@ class NewAuxChannelView extends Component {
     );
   }
 }
-export default withStyles(classes)(NewAuxChannelView);
