148542d01b4fd1f0dd92f1fbec19870100069cb1
Risko <3901301+RiskoFPV@users.noreply.github.com>
2021-06-08 13:35:06 -0400

    re-enable DYNAMIC_FILTER for helio

--- src/Views/FeaturesView/FeaturesView.js
+++ src/Views/FeaturesView/FeaturesView.js
@@ -6,7 +6,9 @@ export default class FeaturesView extends Component {
   constructor(props) {
     super(props);
     let filteredFeatures = this.props.fcConfig.imuf
-      ? props.features.filter(feature => feature.id !== "DYNAMIC_FILTER")
+      ? //? props.features.filter(feature => feature.id !== "DYNAMIC_FILTER")
+        // uncomment above line and comment the one below if we want to disable DYNAMIC_FILTER for helio again
+        props.features
       : props.features;
     this.state = {
       features: filteredFeatures.sort((a, b) => a.id > b.id)
